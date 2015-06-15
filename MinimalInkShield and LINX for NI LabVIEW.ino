dead0c4a2aa8b18cf720058cb43d3333552a8363
atavasoli <alex.tavasoli@gmail.com>
2015-06-15 16:56:26 -0400

    Printer Trial #1- Dot Array
    Runs the MinimalInkShield script to LINX for LabVIEW
    
    VI includes interface to print simple patterns

---
+++ MinimalInkShield and LINX for NI LabVIEW.ino
@@ -0,0 +1,68 @@
+/****************************************************************************************	
+**  This is example LINX firmware for use with the Arduino Uno with the serial 
+**  interface enabled.
+**
+**  For more information see:           www.labviewmakerhub.com/linx
+**  For support visit the forums at:    www.labviewmakerhub.com/forums/linx
+**  
+**  Written By Sam Kristoff
+**
+**  BSD2 License.
+****************************************************************************************/
+
+//Include All Peripheral Libraries Used By LINX
+#include <SPI.h>
+#include <Wire.h>
+#include <EEPROM.h>
+#include <Servo.h>
+
+//Include Device Specific Header From Sketch>>Import Library (In This Case LinxChipkitMax32.h)
+//Also Include Desired LINX Listener From Sketch>>Import Library (In This Case LinxSerialListener.h)
+#include <LinxArduinoUno.h>
+#include <LinxSerialListener.h>
+#include <InkShieldLite.h>
+
+// initialize shield on pin 2 
+const byte pulsePin = 2;
+
+//Create A Pointer To The LINX Device Object We Instantiate In Setup()
+LinxArduinoUno* LinxDevice;
+
+//Initialize LINX Device And Listener
+void setup()
+{
+  //Instantiate The LINX Device
+  LinxDevice = new LinxArduinoUno();
+  
+  //The LINXT Listener Is Pre Instantiated, Call Start And Pass A Pointer To The LINX Device And The UART Channel To Listen On
+  LinxSerialConnection.Start(LinxDevice, 0);  
+  
+  setABCDPinMode(abcdA0A3, OUTPUT);  //set the abcd pins as outputs
+  pinMode(pulsePin, OUTPUT);         //set the pulse pin as output
+}
+
+void loop()
+{
+  //Listen For New Packets From LabVIEW
+  LinxSerialConnection.CheckForCommands();
+  
+  //Your Code Here, But It will Slow Down The Connection With LabVIEW
+  spray_ink(0x0AAA);
+  delay(1000);
+}
+
+void spray_ink(word strip)
+{
+  //loop thru the strip
+  for(byte i = 0; i <= 11; i++){
+    if(strip & 1<<i){
+      fastABCDDigitalWrite(abcdA0A3, i, HIGH);  //set abcd (nozzle address)
+      fastDigitalWrite(pulsePin, HIGH); delayMicroseconds(5);  //pulse pin high, wait 5us
+      fastDigitalWrite(pulsePin, LOW); //pulse pin low
+      fastABCDDigitalWrite(abcdA0A3, i, LOW); //reset abcd
+    }
+  }	
+  //wait to be sure we don't try to fire nozzles too fast and burn them out
+  delayMicroseconds(1000);
+}
+
